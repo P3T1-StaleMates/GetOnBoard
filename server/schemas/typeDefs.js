d11207cfd543687f8d5512225ea3d456f26efbdc
Jesse Comeau <97984389+JesseComeau@users.noreply.github.com>
2022-06-30 17:54:16 -0500

    TypeDefs types and basic queries established.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,23 +1,68 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Profile {
-    _id: ID
-    name: String
-    skills: [String]!
-  }
+	type User {
+		_id: ID!
+		name: String
+		username: String
+		email: String
+		password: String
+		ownedGames: [Game]
+		events: [Event]
+		groups: [Group]
+		friends: [User]
+		posts: [Post]
+	}
 
-  type Query {
-    profiles: [Profile]!
-    profile(profileId: ID!): Profile
-  }
+	type Event {
+		_id: ID!
+		name: String
+		date: Int
+		game: Game
+		winner: User
+	}
 
-  type Mutation {
-    addProfile(name: String!): Profile
-    addSkill(profileId: ID!, skill: String!): Profile
-    removeProfile(profileId: ID!): Profile
-    removeSkill(profileId: ID!, skill: String!): Profile
-  }
+	type Game {
+		_id: ID!
+		name: String
+		description: String
+		genre: String
+		image: String
+		minPlayer: Int
+		maxPlayer: Int
+		averageTime: Int
+	}
+
+	type Group {
+		_id: ID!
+		name: String
+		members: [User] #check models
+		events: [Event]
+	}
+
+	type Post {
+		_id: ID!
+		postText: String!
+		postAuther: User
+		createdAt: String
+	}
+
+type Query {
+	users: [User]
+	user(userId: ID!): User
+	events: [Event]
+	event(eventId: ID!): Event
+	groups: [Group]
+	group(groupId: ID!): Group
+	games: [Game]
+	game(gameId: ID!): Game
+	posts: [Post]
+	post(postId: ID!): Post
+}
+
+type Mutation {
+	addUser(name: String!, username: String!, email: String!, password: String!): User
+}
 `;
 
 module.exports = typeDefs;
