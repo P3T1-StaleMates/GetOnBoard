ab83a49235a799bc9ae816d0c64aa2a860bff2c1
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-07-05 18:38:17 -0500

    added card component styling

--- client/src/components/Cards/GameCard/index.js
+++ client/src/components/Cards/GameCard/index.js
@@ -1,9 +1,24 @@
 // card component for games
 
-const GameCard = () => {
-
+const GameCard = (props) => {
     // needs to display game information
-    return
-}
 
-export default GameCard
+    const { title, players, timeLength, imgUrl } = props;
+
+    return (
+        <div className="card" style={{ width: "15rem" }}>
+            <img
+                src={imgUrl}
+                className="card-img-top"
+                alt="description"
+            />
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{`#ofplayers:${players} gamelength: ${timeLength}`}</p>
+                {/* <a href="#"></a> */}
+            </div>
+        </div>
+    );
+};
+
+export default GameCard;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,103 +1,175 @@
-import React from 'react';
-
+import React from "react";
+import GameCard from "../components/Cards/GameCard";
 
 const Home = () => {
-
-
-  return (
-    <main className="bg-white mt-5 mb-5 mrl-20">
-    <div className="container">
-    <img href="#" className="brand-logo logo" src="assets/images/Get-on-Board.png" />
-    <button type="button" className="btn">Log Out</button>
-    </div>
-    <div className="container">
-        <div className="mt-5">
-            <h5>Hello, John Doe!</h5>
-            <p>Welcome back let's Get on Board</p>
-        </div>
-        <div className="row">
-            <div className="col ">
-                <div className="container ">
-                    <div className="row align-items-center">
-                        <div className="col">
-                    <div className="card" style={{width: "15rem"}}>
-                        <img src="/assets/images/youreonmute.jpg" className="card-img-top" alt="..." />
-                        <div className="card-body">
-                          <h5 className="card-title">Game Title</h5>
-                          <p className="card-text">2 Player - 10 min Game</p>
-                          <a href="#"></a>
-                        </div>
-                      </div>
-                  </div>
-                  <div className="col">
-                    <div className="card" style={{width: "15rem"}}>
-                        <img src="/assets/images/youreonmute.jpg" className="card-img-top" alt="..." />
-                        <div className="card-body">
-                          <h5 className="card-title">Game Title</h5>
-                          <p className="card-text">2 Player - 10 min Game</p>
-                          <a href="#"></a>
-                        </div>
-                      </div>
-                  </div>
-                  <div className="col">
-                    <div className="card" style={{width: "15rem"}}>
-                        <img src="/assets/images/youreonmute.jpg" className="card-img-top" alt="..." />
-                        <div className="card-body">
-                          <h5 className="card-title">Game Title</h5>
-                          <p className="card-text">2 Player - 10 min Game</p>
-                          <a href="#"></a>
-                        </div>
-                      </div>
-                  </div>
+    return (
+        <main className="bg-white mt-5 mb-5 mrl-20">
+            <div className="container">
+                <img
+                    href="#"
+                    className="brand-logo logo"
+                    src="assets/images/Get-on-Board.png"
+                />
+                <button type="button" className="btn">
+                    Log Out
+                </button>
+            </div>
+            <div className="container">
+                <div className="mt-5">
+                    <h5>Hello, John Doe!</h5>
+                    <p>Welcome back let's Get on Board</p>
                 </div>
-
-
-                {/* <!-- end of cards --> */}
-
-
-                <div className="row align-items-center">
-                    <div className="col">
-                      <div className="card" style={{width: "15rem"}}>
-                          <img src="/assets/images/youreonmute.jpg" className="card-img-top" alt="..." />
-                          <div className="card-body">
-                            <h5 className="card-title">Game Title</h5>
-                            <p className="card-text">2 Player - 10 min Game</p>
-                            <a href="#"></a>
-                          </div>
-                        </div>
-                    </div>
-                  
-                    <div className="col">
-                        <div className="card" style={{width: "15rem"}}>
-                            <img src="/assets/images/youreonmute.jpg" className="card-img-top" alt="..." />
-                            <div className="card-body">
-                              <h5 className="card-title">Game Title</h5>
-                              <p className="card-text">2 Player - 10 min Game</p>
-                              <a href="#"></a>
+                <div className="row">
+                    <div className="col ">
+                        <div className="container ">
+                            <div className="row align-items-center">
+                                <div className="col">
+                                  <GameCard title="game title" players="2" timeLength="30 min" imgUrl="/assets/images/youreonmute.jpg"/>
+                                    {/* <div
+                                        className="card"
+                                        style={{ width: "15rem" }}
+                                    >
+                                        <img
+                                            src="/assets/images/youreonmute.jpg"
+                                            className="card-img-top"
+                                            alt="..."
+                                        />
+                                        <div className="card-body">
+                                            <h5 className="card-title">
+                                                Game Title
+                                            </h5>
+                                            <p className="card-text">
+                                                2 Player - 10 min Game
+                                            </p>
+                                            <a href="#"></a>
+                                        </div>
+                                    </div> */}
+                                </div>
+                                <div className="col">
+                                    <div
+                                        className="card"
+                                        style={{ width: "15rem" }}
+                                    >
+                                        <img
+                                            src="/assets/images/youreonmute.jpg"
+                                            className="card-img-top"
+                                            alt="..."
+                                        />
+                                        <div className="card-body">
+                                            <h5 className="card-title">
+                                                Game Title
+                                            </h5>
+                                            <p className="card-text">
+                                                2 Player - 10 min Game
+                                            </p>
+                                            <a href="#"></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col">
+                                    <div
+                                        className="card"
+                                        style={{ width: "15rem" }}
+                                    >
+                                        <img
+                                            src="/assets/images/youreonmute.jpg"
+                                            className="card-img-top"
+                                            alt="..."
+                                        />
+                                        <div className="card-body">
+                                            <h5 className="card-title">
+                                                Game Title
+                                            </h5>
+                                            <p className="card-text">
+                                                2 Player - 10 min Game
+                                            </p>
+                                            <a href="#"></a>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                          </div>
-                      </div>
-                    
 
-                    {/* <!-- solo card --> */}
-                    <div className="col">
-                      <div className="card" style={{width: "15rem"}}>
-                          <img src="/assets/images/dice-and-pawns.jpg" className="card-img-top" alt="..." />
-                          <div className="card-body">
-                            <h5 className="card-title">LET'S <br/> EXPLORE!</h5>
-                            <p className="card-text">Find and add more games</p>
-                            <button type="button" className="btn center">EXPLORE</button>
-                          </div>
+                            {/* <!-- end of cards --> */}
+
+                            <div className="row align-items-center">
+                                <div className="col">
+                                    <div
+                                        className="card"
+                                        style={{ width: "15rem" }}
+                                    >
+                                        <img
+                                            src="/assets/images/youreonmute.jpg"
+                                            className="card-img-top"
+                                            alt="..."
+                                        />
+                                        <div className="card-body">
+                                            <h5 className="card-title">
+                                                Game Title
+                                            </h5>
+                                            <p className="card-text">
+                                                2 Player - 10 min Game
+                                            </p>
+                                            <a href="#"></a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col">
+                                    <div
+                                        className="card"
+                                        style={{ width: "15rem" }}
+                                    >
+                                        <img
+                                            src="/assets/images/youreonmute.jpg"
+                                            className="card-img-top"
+                                            alt="..."
+                                        />
+                                        <div className="card-body">
+                                            <h5 className="card-title">
+                                                Game Title
+                                            </h5>
+                                            <p className="card-text">
+                                                2 Player - 10 min Game
+                                            </p>
+                                            <a href="#"></a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                {/* <!-- solo card --> */}
+                                <div className="col">
+                                    <div
+                                        className="card"
+                                        style={{ width: "15rem" }}
+                                    >
+                                        <img
+                                            src="/assets/images/dice-and-pawns.jpg"
+                                            className="card-img-top"
+                                            alt="..."
+                                        />
+                                        <div className="card-body">
+                                            <h5 className="card-title">
+                                                LET'S <br /> EXPLORE!
+                                            </h5>
+                                            <p className="card-text">
+                                                Find and add more games
+                                            </p>
+                                            <button
+                                                type="button"
+                                                className="btn center"
+                                            >
+                                                EXPLORE
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                  </div>
-                  </div>
                 </div>
-              </div>
-          </div>
-
-</main>
-  );
+            </div>
+        </main>
+    );
 };
 
 export default Home;
