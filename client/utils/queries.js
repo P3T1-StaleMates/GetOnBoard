5dc8e5197c40cc832f8ebd027424a487dae55778
Jesse Comeau <97984389+JesseComeau@users.noreply.github.com>
2022-07-05 17:58:30 -0500

    Added mutations and queries to the client side utils folder.

---
+++ client/utils/queries.js
@@ -0,0 +1,130 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_PLAYERS = gql`
+  query players {
+  players {
+    _id
+    name
+    username
+    ownedGames {
+      _id
+      name
+    }
+    groups {
+      _id
+      name
+    }
+    friends {
+      _id
+      name
+    }
+  }
+}
+`;
+
+export const QUERY_PLAYER = gql`
+  query player($username: String!) {
+  player(username: $username) {
+    _id
+    name
+    username
+    email
+    password
+    ownedGames {
+      _id
+      name
+      description
+      genre
+    }
+    groups {
+      _id
+      name
+      admin {
+        _id
+        name
+      }
+    }
+  }
+}
+`;
+
+export const QUERY_ME = gql`
+  query me($id: ID!) {
+  me(_id: $id) {
+    _id
+    name
+    username
+    email
+    password
+    ownedGames {
+      _id
+      name
+      description
+      genre
+    }
+    groups {
+      _id
+      name
+    }
+    friends {
+      _id
+      name
+    }
+  }
+}
+`;
+
+export const QUERY_GROUPS = gql`
+  query groups {
+  groups {
+    _id
+    name
+    admin {
+      _id
+      name
+      username
+    }
+    members {
+      _id
+      name
+      ownedGames {
+        _id
+        name
+      }
+    }
+  }
+}
+`;
+
+export const QUERY_GROUP = gql`
+  query group($id: ID!) {
+  group(_id: $id) {
+    _id
+    name
+    admin {
+      _id
+      name
+      username
+    }
+    members {
+      _id
+      name
+      username
+    }
+    events {
+      _id
+      owner {
+        _id
+        name
+      }
+      name
+      players {
+        _id
+        name
+      }
+      location
+      date
+    }
+  }
+}
+`;
