e0f356d81c543906de95f6a92a88ec5f72bbef74
lealinnea <leag0905@gmail.com>
2022-07-05 21:05:44 -0500

    adding changes to components

--- client/src/components/Cards/EventCard/index.js
+++ client/src/components/Cards/EventCard/index.js
@@ -1,10 +1,26 @@
+import React from 'react';
+
+
 // card for displaying upcoming events for the user / group
 
 const EventCard = () => {
     return
 }
-
-
+<div className="card mb-3" style="max-width: 370px;">
+<div className="row g-0">
+  <div className="col-md-2">
+   <img></img>
+  </div>
+  <div className="col-md-8">
+    <div className="card-body">
+      <h5 className="card-title">Meeting</h5>
+      <p className="card-text">Meeting with Chris</p>
+      <p className="card-text"><small class="text-muted">July 3, at 10pm</small></p>
+    </div>
+  </div>
+</div>
+</div>
+       
 
 
 export default EventCard
