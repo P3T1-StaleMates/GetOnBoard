88ddbcbe468678a5d14b7d8da79fc64555505362
lealinnea <leag0905@gmail.com>
2022-07-06 21:37:41 -0500

    routing

--- client/src/components/Global/Footer/index.js
+++ client/src/components/Global/Footer/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer>
-      <div>
-        <p>&copy; 2022 - StaleMates</p>
+    <div className="container">
+      <div className="row">
+        <section></section>
       </div>
-    </footer>
+      <div className="row">
+        <div className="col">
+          <h3 className="padding-top-40">PUT AD HERE</h3>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,7 +1,34 @@
-// user dashboard - holds other displays inside
+import GameCard from "../components/Cards/GameCard";
+import PlayerCard from "../components/Cards/PlayerCard";
+import EventCard from "../components/Cards/EventCard";// user dashboard - holds other displays inside
 
 const PlayerDash = () => {
-    return
+    return(
+        <>
+         <section className="padding-40">
+        <div className="col"><img className="icon" src="/assets/images/man-icon.jpg" alt="Avatar"/>
+          <div>
+          <h5>Hello, John Doe!</h5>
+          <p>Welcome back let's Get on Board</p>
+          </div>
+          </div>
+        </section>
+        
+        <section>
+          <div className="container">
+            <div className="row">
+              <div className="col"> <GameCard/></div>
+              <div className="col"><GameCard/></div>
+              <div className="col"><GameCard/></div>
+            </div>
+            <div className="row">
+              <div className="col-8">large-card</div>
+              <div className="col-4">small-card</div>
+            </div>
+          </div>
+        </section>
+        </>
+    )
 }
 
 export default PlayerDash
