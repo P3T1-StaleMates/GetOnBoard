2d702919720fac529e7bfbff563dc73aa3363a22
Miguel Escobar <mae2136@gmail.com>
2022-07-08 19:40:59 -0500

    Updated createEvent for single use only.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -91,7 +91,7 @@ type Mutation {
 	removeGame(gameId: ID!): Player
 
 	#Event Mutations
-	createEvent(name: String!, gameId: ID!, location: String!, date: String!, groupId: ID!): Event
+	createEvent(name: String!, gameId: ID!, location: String!, date: String!, groupId: ID!, players: [String]!): Event
 	deleteEvent(eventId: ID!, groupId: ID!): Event
 	addEventWinner(eventId: ID!, winnerId: ID!): Event
 
