e922219aa002c99bbc77ac68c1d407e37dce1e31
lealinnea <leag0905@gmail.com>
2022-07-12 21:10:22 -0500

    my games

--- client/src/components/Cards/GameCard/index.js
+++ client/src/components/Cards/GameCard/index.js
@@ -1,7 +1,21 @@
 import "./GameCard.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { REMOVE_GAME } from "../../../utils/mutations";
+import { useMutation } from '@apollo/client';
 // card component for games
 
 const GameCard = ({ game }) => {
+    const [removeGame, { error }] = useMutation(REMOVE_GAME);
+
+  const handleRemoveGame = async (gameId) => {
+    try {
+      const { removedGame } = await removeGame({
+        variables: { gameId }
+      })
+    } catch (err) {
+      console.error(err);
+    }
+  }
 let stockImage = "https://image.shutterstock.com/image-photo/letter-tiles-spelling-out-words-260nw-1509215651.jpg"
     return (
         <div className="card game" style={{ width: "15rem" }}>
@@ -9,6 +23,8 @@ let stockImage = "https://image.shutterstock.com/image-photo/letter-tiles-spelli
             <div className="card-body">
                 <h5 className="card-title">Title of game: {game.title}</h5>
                 <p className="card-text">{`#ofplayers: ${`${game.minPlayer} - ${game.maxPlayer}`} Gamelength: ${game.averageTime}`}</p>
+                <button className="btn-danger" onClick={() => handleRemoveGame(game._id)}><FontAwesomeIcon icon="fa-solid fa-trash fa-2xl" /></button>
+            
             </div>
         </div>
     );
--- client/src/pages/MyGames.js
+++ client/src/pages/MyGames.js
@@ -1,22 +1,12 @@
 import GameCard from "../components/Cards/GameCard";
 import Searchbar from "../components/Searchbar";
-import { useMutation, useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import { QUERY_ME } from "../utils/queries";
-import { REMOVE_GAME } from "../utils/mutations";
+
 
 const MyGames = () => {
   const { loading, data } = useQuery(QUERY_ME);
-  const [removeGame, { error }] = useMutation(REMOVE_GAME);
-
-  const handleRemoveGame = async (gameId) => {
-    try {
-      const { removedGame } = await removeGame({
-        variables: { gameId }
-      })
-    } catch (err) {
-      console.error(err);
-    }
-  }
+  
 
   if (loading) {
     return <div>Loading....</div>
@@ -41,7 +31,7 @@ const MyGames = () => {
           <div className="row ">
             {ownedGames.length ? ownedGames.map((game) => {
               return <div className="col" key={game.title}> <GameCard game={game} />
-                <button className="btn btn-danger" onClick={() => handleRemoveGame(game._id)}>Remove Game</button>
+               
               </div>
             }) : <p>No Games to Show! Add a new game by searching for one above!</p>}
           </div>
