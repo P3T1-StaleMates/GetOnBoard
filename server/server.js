b5e6fca01bd43e5f11d0a38df72446f1fa0b6f68
Miguel Escobar <mae2136@gmail.com>
2022-07-12 23:37:17 -0500

    Added nodemailer package to createEvent.
    Todo: implement dotenv on backend properly.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+const dotenv = require('dotenv');
 
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
