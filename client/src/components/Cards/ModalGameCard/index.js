a83eb8e8ab561d27774ca7ef1de8223b64e6c0d5
Jesse Comeau <97984389+JesseComeau@users.noreply.github.com>
2022-07-14 16:19:17 -0500

    Updates to game cards.

--- client/src/components/Cards/ModalGameCard/index.js
+++ client/src/components/Cards/ModalGameCard/index.js
@@ -7,14 +7,22 @@ import "./ModalGameCard.css"
 
 const ModalGameCard = ({ game }) => {
 
-let stockImage = "https://image.shutterstock.com/image-photo/letter-tiles-spelling-out-words-260nw-1509215651.jpg"
+    let stockImage = "https://image.shutterstock.com/image-photo/letter-tiles-spelling-out-words-260nw-1509215651.jpg"
     return (
-        <div className="card transparent game" style={{ width: "15rem" }}>
-            <img src={game.imageUrl || stockImage} className="card-img-top" alt="description" />
-            <div className="card-body">
-                <h5 className="card-title">{game.title}</h5>
-                <p className="card-text">{`Players: ${`${game.minPlayer} - ${game.maxPlayer}`} `}</p>
-                <p>{`Game Length: ${game.averageTime} min`}</p>
+        <div className=" gameDash col-4" style={{ width: "15rem" }}>
+            <div className="team-member mt-2">
+
+                <div className="team-img d-flex justify-content-center">
+
+                    <img src={game.imageUrl || stockImage} className="card-img-top img-responsive-dash" alt="description"></img>
+                </div>
+                <div className="game-hover">
+                    <div className="desk align-content-center">
+                        <h5 className="card-title">{game.title}</h5>
+                        <p className="card-text">{`Players: ${`${game.minPlayer} - ${game.maxPlayer}`} `}</p>
+                        <p>{`Game Length: ${game.averageTime} min`}</p>
+                    </div>
+                </div>
 
 
             </div>
