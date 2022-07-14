556515216a171c65c804b5421ddd78102c32ac52
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-07-13 19:33:28 -0500

    formatting

--- client/src/pages/MyGames.js
+++ client/src/pages/MyGames.js
@@ -1,54 +1,64 @@
 import GameCard from "../components/Cards/GameCard";
 import Searchbar from "../components/Searchbar";
-import { useQuery } from '@apollo/client';
+import { useQuery } from "@apollo/client";
 import { QUERY_ME } from "../utils/queries";
 
-
 const MyGames = () => {
-  const { loading, data } = useQuery(QUERY_ME);
-  
+    const { loading, data } = useQuery(QUERY_ME);
 
-  if (loading) {
-    return <div>Loading....</div>
-  }
+    if (loading) {
+        return <div>Loading....</div>;
+    }
 
-  const { ownedGames } = data.me
+    const { ownedGames } = data.me;
 
-  return (
-    <>
-    <div class="container">
-  <div class="row">
-    <div class="col padding-40">
-        <div><img className="icon p-2" src="/assets/images/man-icon.jpg" alt="Avatar" />
-          <div>
-            <h5>My Games</h5>
-            <p>View all your added games</p>
-          </div>
-      
-      </div>
-      </div>
-      <div class="col p-3">
-        <Searchbar />
-        </div>
+    return (
+        <>
+            <div className="container">
+                <div className="row">
+                    <div className="col padding-40">
+                        <div>
+                            <img
+                                className="icon p-2"
+                                src="/assets/images/man-icon.jpg"
+                                alt="Avatar"
+                            />
+                            <div>
+                                <h5>My Games</h5>
+                                <p>View all your added games</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col p-3">
+                        <Searchbar />
+                    </div>
+                </div>
+            </div>
 
-      </div>
-      </div>
+            <section className="overflow">
+                <div className="container ">
+                    <div className="row ">
+                        {ownedGames.length ? (
+                            ownedGames.map((game) => {
+                                return (
+                                    <div className="col" key={game.title}>
+                                        {" "}
+                                        <GameCard game={game} />
+                                    </div>
+                                );
+                            })
+                        ) : (
+                            <p>
+                                No Games to Show! Add a new game by searching
+                                for one above!
+                            </p>
+                        )}
+                    </div>
+                    <div className="row"></div>
+                </div>
+            </section>
+        </>
+    );
+};
 
-      <section className="overflow">
-        <div className="container ">
-          <div className="row ">
-            {ownedGames.length ? ownedGames.map((game) => {
-              return <div className="col" key={game.title}> <GameCard game={game} />
-               
-              </div>
-            }) : <p>No Games to Show! Add a new game by searching for one above!</p>}
-          </div>
-          <div className="row">
-          </div>
-        </div>
-      </section>
-    </>
-  )
-}
-
-export default MyGames
+export default MyGames;
